eebe0e247d02c78324e9093125924185fda11a76
PlerKudaZz <kudap9744@gmail.com>
2024-09-29 21:08:00 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Pesan Anda telah dikirim!');
-    this.reset(); // Reset form setelah pengiriman
-});
